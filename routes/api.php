c3931c8c826c6d5aa869b96ac49ead7cba942467
CesarLinux <eduardomelchor34@gmail.com>
2025-03-20 23:10:15 -0600

    Getting products

--- routes/api.php
+++ routes/api.php
@@ -3,16 +3,5 @@
 use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
-Route::get("/test", function(){
-  return "el backend funciona correctamente";
-});
 
-Route::get("/product", [ProductController::class, "getAll"]);
-
-Route::get("/product/{id?}", [ProductController::class, "get"]);
-
-Route::post("/product", [ProductController::class, "create"]);
-
-Route::put("/product/{id}", [ProductController::class, "update"]);
-
-Route::delete("/product/{id}", [ProductController::class, "delete"]);
+Route::get("/product", [ProductController::class, "get"]);
